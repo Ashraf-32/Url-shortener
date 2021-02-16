4adc252f0968afa2a756ed0c7b2f7b4573868ce9
Ashraf-32 <syedishmam12@gmail.com>
2021-02-16 13:37:19 +0600

    added the login system

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Url(models.Model):
     url_id = models.AutoField(primary_key=True)
-    url = models.CharField(max_length=700)
+    url = models.CharField(max_length=700, unique=True)
     short = models.CharField(max_length=20)
     date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return str(self.url)
--- shorturl/urls.py
+++ shorturl/urls.py
@@ -17,6 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:id>/', views.redirect_url, name='redirect'),
+    path('', views.index, name='home'),
+    path('<str:id>/', views.redirect_url, name='redirect_url'),
 ]
